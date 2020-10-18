314cbd824ee9bd7f81cc6784a39ea13a170e648d
Sai Prashanth <saiprashanthgunukula@gmail.com>
2020-10-18 18:22:07 +0530

    Add firebase config

---
+++ src/config/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: "mobile-store-bcba7.firebaseapp.com",
+    databaseURL: process.env.DATABASE_URL,
+    projectId: "mobile-store-bcba7",
+    storageBucket: "mobile-store-bcba7.appspot.com",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+
+const fire = firebase.initializeApp(firebaseConfig)
+
+export default fire;
