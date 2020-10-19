892e8ee0b552e6e40d34c17b83fdbb8d84b13f6a
Sai Prashanth <saiprashanthgunukula@gmail.com>
2020-10-19 10:52:42 +0530

    Add firebase environment variables

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: "mobile-store-bcba7.firebaseapp.com",
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: "mobile-store-bcba7",
-    storageBucket: "mobile-store-bcba7.appspot.com",
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
+  apiKey: "AIzaSyBuHAiYkJI0iKYLUlY0y8pM5ClQapNv8eg",
+  authDomain: "mobile-store-bcba7.firebaseapp.com",
+  databaseURL: "https://mobile-store-bcba7.firebaseio.com",
+  projectId: "mobile-store-bcba7",
+  storageBucket: "mobile-store-bcba7.appspot.com",
+  messagingSenderId: "404487634481",
+  appId: "1:404487634481:web:e81f5882d972679fcb9f10",
+  measurementId: "G-CPQV69ESJ6"
+};
 
 const fire = firebase.initializeApp(firebaseConfig)
 
